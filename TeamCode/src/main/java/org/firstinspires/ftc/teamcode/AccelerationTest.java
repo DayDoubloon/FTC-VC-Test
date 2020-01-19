9a2a20c66c495280f543890e1e422d16a6b6c617
DayDoubloon <cjsoloff@icloud.com>
2020-01-18 23:02:36 -0500

    Started designing a acceleration and deceleration function for movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AccelerationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AccelerationTest.java
@@ -21,37 +21,57 @@ public class AccelerationTest extends LinearOpMode {
 
     }
 
-    public void accelDecel(int targetPosition, double minSpeed, double maxSpeed) {
+    public void accelDecel(int targetPosition, double maxSpeed) {
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         motor.setTargetPosition(targetPosition);
 
         double accelerationDistance = motor.getTargetPosition() / 4;
         double decelerationDistance = motor.getTargetPosition() - accelerationDistance;
+        double power;
 
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         while (opModeIsActive() && motor.getCurrentPosition() < motor.getTargetPosition()) {
 
             if (motor.getCurrentPosition() < accelerationDistance) {
-
                 double speedFactor = motor.getCurrentPosition() / accelerationDistance;
-                motor.setPower(speedFactor * maxSpeed);
+                power = speedFactor * maxSpeed;
+
+                if (power < .1) {
+                    power = .1;
+                }
+
+                motor.setPower(power);
 
             } else if (decelerationDistance > motor.getCurrentPosition() && motor.getCurrentPosition() > accelerationDistance) {
+                power = maxSpeed;
 
-                motor.setPower(maxSpeed);
+                if (power < .1) {
+                    power = .1;
+                }
+
+                motor.setPower(power);
 
             } else if (motor.getCurrentPosition() > decelerationDistance) {
-
                 double speedFactor = (motor.getCurrentPosition() - decelerationDistance) / accelerationDistance;
-                motor.setPower(speedFactor * maxSpeed);
+                power = speedFactor * maxSpeed;
+
+                if (power < .1) {
+                    power = .1;
+                }
+
+                motor.setPower(power);
 
             }
 
         }
 
         motor.setPower(0);
+
+    }
+
+    public void continuousAccelDecel() {
 
     }
 
